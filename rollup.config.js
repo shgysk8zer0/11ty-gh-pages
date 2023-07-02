f033d6b1707019e39882477e502f5a84e0455cd3
Chris Zuber <shgysk8zer0@gmail.com>
2023-07-02 09:00:39 -0700

    Initial commit

---
+++ rollup.config.js
@@ -0,0 +1,14 @@
+/* eslint-env node */
+import { getConfig } from '@shgysk8zer0/js-utils/rollup';
+import { rollupImport, rollupImportMeta } from '@shgysk8zer0/rollup-import';
+import { importmap } from '@shgysk8zer0/importmap';
+
+export default getConfig('./js/index.js', {
+	plugins: [
+		rollupImport(importmap),
+		rollupImportMeta({ baseURL: 'https://dev.kernvalley.us/' }),
+	],
+	format: 'iife',
+	minify: true,
+	sourcemap: true,
+});
